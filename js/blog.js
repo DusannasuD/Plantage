9e4eff457362250f6f1edeec730788a120c407e0
Dusan <60298038+DusannasuD@users.noreply.github.com>
2020-09-30 16:25:40 +0200

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,4 @@
+
 // Show Newsletter form on click
 $(document).ready(function(){
   $("#newsletter_btn").click(function(){
@@ -11,3 +12,36 @@ $(document).ready(function(){
     $("#newsletter_form").fadeOut(500);
   });
 });
+
+// Subscribe to Newsletter
+let submitButton = document.getElementById("addPost");
+submitButton.addEventListener("submit", addPost);
+
+function addPost(e) {
+  e.preventDefault();
+
+  let name = document.getElementById("fname").value;
+  let mail = document.getElementById("mail").value;
+
+  fetch("http://localhost:3000/subscribers", { 
+  method: "POST",
+  headers: {
+    "Content-type": "application/json"
+  },
+  body: JSON.stringify({
+    nameAndSurname: name, 
+    email: mail
+    })
+  })
+  .then((res) => res.json())
+  .then((data) => (obj = data))
+  .then(function message() {
+    if (obj.statusCode) {
+      alert("Greška! Ime ili email već postoji.");
+      window.location.href = "blog.html";
+    } else {
+      alert("Uspešno ste se prijavili na Newsletter!");
+      window.location.href = "blog.html";
+    }
+  })
+}
