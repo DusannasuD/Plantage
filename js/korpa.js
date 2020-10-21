394ffcc61483d5a16b086a7e005414e4856b1c74
Dusan <60298038+DusannasuD@users.noreply.github.com>
2020-10-21 20:24:36 +0200

    Update korpa.js

--- js/korpa.js
+++ js/korpa.js
@@ -8,23 +8,25 @@ if (document.readyState == "loading") {
 function ready() {
   let prods = localStorage.getItem("productsInCart");
   let products = JSON.parse(prods);
-  let cartNum = localStorage.getItem("cartNumbers");
-  let number = JSON.parse(cartNum);
-  let total = JSON.parse(localStorage.getItem("totalCost"));
 
-  checlLocalStorage();
+  checkLocalStorage();
   cartItem(products);
-  totalPrice(total);
+  updateTotal();
 }
 
 // Check Local Storege on load and update cart number
-function checlLocalStorage() {
-  let cartNumber = localStorage.getItem("cartNumbers");
-  cartNumber - JSON.parse(cartNumber);
-  
-  if (cartNumber) {
-    document.getElementById("cart").style.display = "block";
-    document.getElementById("cart").textContent = cartNumber;
+function checkLocalStorage() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems) {
+    let sum = 0;
+
+    for (let i = 0; i < cartItems.length; i++) {
+      sum += cartItems[i].quan;
+      document.getElementById("cart").style.display = "block";
+      document.getElementById("cart").textContent = sum;
+    }
   }
 }
 
@@ -48,32 +50,40 @@ function cartItem(products) {
 
       cartItem.innerHTML += `
         <div class="productInCart" style="display: flex; flex-directoin: row; padding: 15px 0;">
+          <!-- Remove button -->
           <div class="odabrani-proizvod" style="flex-shrink: 30;">
-            <span><i class="fa fa-times-circle" style="font-size: 25px;"></i></span>
+            <span><i class="fa fa-times-circle" style="font-size: 25px;" onclick="removeItem(${product.id})"></i></span>
           </div>
 
+          <!-- Product image -->
           <div class="odabrani-proizvod">
             <img src="${product.img}" width="80%" class="picture">
           </div>
 
+          <!-- Product price -->
           <div class="odabrani-proizvod">
-            <p><span><strong style="font-size: 20px; font-weight: normal;" class="prod-price">
-            ${product.prodPrice},00</strong></span></p>
+            <p><span class="single-price"><strong style="font-size: 20px; font-weight: normal;" class="prod-price">
+            ${product.prodPrice}</strong></span></p>
           </div>
 
+          <!-- Quantity -->
           <div class="odabrani-proizvod"> 
             <div style="display: flex; flex-direction: row">
               <button style="margin-right: 8px; background-color: white; display: flex; flex-direction: row; 
               align-items: center; padding: 3px 20px;">
-                <span style="padding-right: 30px; padding-left: 10px;" class="quantity">1</span>
-                <span style= "display: flex; flex-direction: column;"><i class="fa fa-angle-up" style="padding-bottom: 2px;"></i>
-                <i class="fa fa-angle-down"></i></span>
+                <span style="padding-right: 30px; padding-left: 10px;" class="quantity">${product.quan}</span>
+                <span style= "display: flex; flex-direction: column;" class="kol">
+                  <i class="fa fa-angle-up" style="padding-bottom: 2px;" onclick="quantityUp(${product.id})"></i>
+                  <i class="fa fa-angle-down" onclick="quantityDown(${product.id})"></i>
+                </span>
               </button>
             </div>
           </div>
 
+          <!-- Sigle product total price -->
           <div class="odabrani-proizvod">
-            <p><span id="ukupno" style="font-size: 20px; font-weight: normal;"></span></p>
+            <p><span class="ukupno" style="font-size: 20px; font-weight: normal;">
+            ${product.prodPrice * product.quan}</span></p>
           </div>
         </div>
         <hr>
@@ -124,25 +134,107 @@ function cartItem(products) {
     ` 
 }
 
-function totalPrice(total) {
+// Total price
+function updateTotal() {
+  let total = JSON.parse(localStorage.getItem("totalCost"));
   
   if (total) {
-    document.getElementsByClassName("total-price")[0].textContent = total + ",00" + " + poštarina";
+    document.getElementsByClassName("total-price")[0].textContent = total + " + poštarina";
   }
+}
 
+// Increase quantity 
+function quantityUp(id) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let cartCost = localStorage.getItem("totalCost");
+  cartCost = parseInt(cartCost);
+  let price = event.target.parentElement.parentElement.parentElement.parentElement.previousElementSibling.
+  firstElementChild.firstElementChild.textContent;
+  price = parseInt(price);
+
+  cartItems.forEach(item => {
+  let singleProdPrice = event.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.
+  firstElementChild.textContent;
+  singleProdPrice = parseInt(singleProdPrice);
+
+    if (item.id == id) {
+      item.quan++;
+      event.target.parentElement.parentElement.firstElementChild.textContent++;
+      event.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.
+      firstElementChild.textContent = parseInt(singleProdPrice) + price;
+    }
+  });
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+  localStorage.setItem("totalCost", cartCost + price);
+  checkLocalStorage();
+  updateTotal();
+}
+
+// Decrease quantity
+function quantityDown(id) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let cartCost = localStorage.getItem("totalCost");
+  cartCost = parseInt(cartCost);
+  let price = event.target.parentElement.parentElement.parentElement.parentElement.previousElementSibling.
+  firstElementChild.firstElementChild.textContent;
+  price = parseInt(price);
+
+  cartItems.forEach(item => {
+  let singleProdPrice = event.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.
+  firstElementChild.textContent;
+  singleProdPrice = parseInt(singleProdPrice);
+
+    if (item.id == id) {
+
+      if (item.quan > 1) {
+        item.quan--;
+        event.target.parentElement.parentElement.firstElementChild.textContent--;
+        event.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling.firstElementChild.
+        firstElementChild.textContent = parseInt(singleProdPrice) - price;
+        localStorage.setItem("totalCost", cartCost - price);
+      }
+    }
+  });
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+  checkLocalStorage();
+  updateTotal();
+}
+
+// Remove product
+function removeItem(id) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let cartCost = localStorage.getItem("totalCost");
+  cartCost = parseInt(cartCost);
+
+  cartItems.forEach(item => {
+    let prod = event.target.parentElement.parentElement.parentElement;
+    let line = event.target.parentElement.parentElement.parentElement.nextElementSibling;
+    let price = event.target.parentElement.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.
+    nextElementSibling.textContent;
+    price = parseInt(price);
+
+    if (item.id == id) {
+      prod.remove();
+      line.remove();
+      localStorage.setItem("totalCost", cartCost - price);
+      cartItems.shift(item);
+    }
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+  });
+
+  updateTotal();
 }
 
 // Show Newsletter form on click
 $(document).ready(function(){
   $("#newsletter_btn").click(function(){
     $("#newsletter_form").fadeIn(500);
-  });
-});
-
-//Close Newsletter form
-$(document).ready(function(){
-  $(".close_form").click(function(){
-    $("#newsletter_form").fadeOut(500);
   });
 });
 
