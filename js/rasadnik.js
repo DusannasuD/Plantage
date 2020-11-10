61cb27d728b1a0dfaafa3cf2b6bb1870c3a631d3
Dusan <60298038+DusannasuD@users.noreply.github.com>
2020-11-10 18:50:20 +0100

    Add files via upload

---
+++ js/rasadnik.js
@@ -0,0 +1,73 @@
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready();
+}
+
+function ready() {
+  checkLocalStorage();
+}
+
+// Check Local Storege on load and update cart number
+function checkLocalStorage() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  
+  if (cartItems) {
+    let sum = 0;
+
+    for (let i = 0; i < cartItems.length; i++) {
+      sum += cartItems[i].quan;
+      document.getElementById("cart").style.display = "block";
+      document.getElementById("cart").textContent = sum;
+    }
+  }
+}
+
+// Show Newsletter form on click
+$(document).ready(function(){
+  $("#newsletter_btn").click(function(){
+    $("#newsletter_form").fadeIn(500);
+  });
+});
+
+//Close Newsletter form
+$(document).ready(function(){
+  $(".close_form").click(function(){
+    $("#newsletter_form").fadeOut(500);
+  });
+});
+
+// Subscribe to Newsletter
+let submitButton = document.getElementById("addPost");
+submitButton.addEventListener("submit", addPost);
+
+function addPost(e) {
+  e.preventDefault();
+
+  let name = document.getElementById("fname").value;
+  let mail = document.getElementById("mail").value;
+
+  fetch("http://localhost:3000/subscribers", { 
+  method: "POST",
+  headers: {
+    "Content-type": "application/json"
+  },
+  body: JSON.stringify({
+    nameAndSurname: name, 
+    email: mail
+    })
+  })
+  .then((res) => res.json())
+  .then((data) => (obj = data))
+  .then(function message() {
+    if (obj.statusCode) {
+      alert("Greška! Ime ili email već postoji.");
+      window.location.href = "rasadnik.html";
+    } else {
+      alert("Uspešno ste se prijavili na Newsletter!");
+      window.location.href = "rasadnik.html";
+    }
+  })
+}
